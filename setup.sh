03b09c57fcf9cf16a32e91a8a2642b59af03ece0
Øyvind Ødegård <oyvind.odegard@schibsted.no>
2020-03-04 08:26:54 +0100

    Add install steps

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+kind delete cluster
 kind create cluster
 
 kubectl create namespace monitoring
 helm install grafana stable/grafana --namespace monitoring
+# TODO add steps for installing prometheus-operator
 
 echo "[INFO]: Grafana username: admin"
 echo "[INFO]: Grafana password: $(kubectl get secret --namespace monitoring grafana -o jsonpath="{.data.admin-password}" | base64 --decode)"
